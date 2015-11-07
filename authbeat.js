c15763552fcccd39d43057b983e256e25cc0af17
Jack Mulrow <jackmulrow@Jacks-MBP-4.wireless.yale.edu>
2015-11-06 20:34:29 -0500

    Added generated files

---
+++ authbeat.js
@@ -0,0 +1,20 @@
+
+
+// file for the authbeat api
+
+// have user put authbeat id on the login input field
+var loginField = document.getElementById("authbeat");
+
+loginField.addEventListener('keydown', function() {
+	//
+
+});
+
+
+
+module.exports = {
+	authenticate: function(old, new) {
+		//////
+		return 
+	}
+}
